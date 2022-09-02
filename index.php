bb739e261877667223f2433346bbadd3f4cdeb47
ViniFernandesDev <vinifernandesdeveloper@gmail.com>
2022-09-02 17:02:30 -0300

    Create section Archives

--- index.php
+++ index.php
@@ -83,4 +83,59 @@
      </div><!--container-->
 </section><!--services-->
 
+<section class="archives">
+     <div class="container">
+
+          <h1 class="titleMain">Archives</h1>
+
+          <div class="__display"> 
+               <div class="__item">
+                    <img src="assets/img/bolo.png" alt="Person">
+
+                    <div class="__txt">
+                         <h2>Why do I eat a lot of cake?</h2>
+                         <a href="">READ MORE >></a>
+                    </div><!--__txt-->
+
+                    <div class="__information">
+                         <span>August 1, 2020</span>
+                         <span>|</span>
+                         <span>No Comments</span>
+                    </div><!--__information-->
+               </div><!--__item-->
+
+               <div class="__item">
+                    <img src="assets/img/bolo.png" alt="Person">
+
+                    <div class="__txt">
+                         <h2>Why does my girlfriend make so much cake?</h2>
+                         <a href="">READ MORE >></a>
+                    </div><!--__txt-->
+
+                    <div class="__information">
+                         <span>August 1, 2020</span>
+                         <span>|</span>
+                         <span>No Comments</span>
+                    </div><!--__information-->
+               </div><!--__item-->
+
+               <div class="__item">
+                    <img src="assets/img/bolo.png" alt="Person">
+
+                    <div class="__txt">
+                         <h2>Should I bring cake for colleagues?</h2>
+                         <a href="">READ MORE >></a>
+                    </div><!--__txt-->
+
+                    <div class="__information">
+                         <span>August 1, 2020</span>
+                         <span>|</span>
+                         <span>No Comments</span>
+                    </div><!--__information-->
+               </div><!--__item-->
+          </div><!--__display-->
+
+     </div><!--container-->
+</section><!--services-->
+
 <?php include('footer.php'); ?>
