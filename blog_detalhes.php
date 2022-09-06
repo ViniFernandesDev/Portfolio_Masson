4b996907a7667a360a59b74fcc93645a7815b855
ViniFernandesDev <vinifernandesdeveloper@gmail.com>
2022-09-06 11:41:37 -0300

    Create porfolio detalhes and blog detalhes

---
+++ blog_detalhes.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    
+     <?php include('meta_tags.php'); ?>
+        
+</head>
+<body>
+
+<?php include('header.php'); ?>
+
+<section class="blogDetails">
+     <div class="container">
+
+          <h1 class="titleMain">Criando sua primeira função no banco de dados</h1>
+
+          <div class="__imgDestaq">          
+               <img src="assets/img/blog_detalhes.jpg" alt="">
+          </div><!--__imgDestaq-->
+
+          <div class="__info">
+            <div class="__author">
+                <img src="assets/img/person_ico.png" alt="">
+                Por: Reginaldo José da silva
+            </div><!--__author-->
+
+            <div class="__date">
+                Dia 01/05/2018 09h00
+            </div><!--__date-->
+          </div><!--__info-->
+
+          <h2>Desmitifique função no banco de dados e utiliza a favor da sua aplicação</h2>
+          
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+     </div><!--container-->
+</section><!--blogDetails-->
+
+<?php include('footer.php'); ?>
---
+++ portfolio_detalhes.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    
+     <?php include('meta_tags.php'); ?>
+        
+</head>
+<body>
+
+<?php include('header.php'); ?>
+
+<section class="portfolioDetails">
+     <div class="container">
+
+          <h1 class="titleMain">Pot Cake</h1>
+          <h2 class="subTitle">Ui / Ux</h2>
+
+          <div class="__display">
+            <ul>
+
+                <li>
+                    <span>Client</span>
+                    <span><mark>Cuckoo</mark></span>
+                </li>
+
+                <li>
+                    <span>Services</span>
+                    <span><mark>UI/UX</mark></span>
+                </li>
+
+                <li>
+                    <span>Date</span>
+                    <span><mark>Feb 10-2019</mark></span>
+                </li>
+
+                <li>
+                    <span>View</span>
+                    <span><mark>Launch Project</mark></span>
+                </li>
+
+            </ul>
+            
+            <div class="collaborators">
+                <div class="__button">
+                    <i class="uil uil-sun"></i>
+                    Collaborators
+                </div><!--__button-->
+
+                <div class="__tooltip">
+                    <a href="">
+                        <div class="__img">
+                            <img src="img/person_ico.png" alt="">
+                        </div><!--__img-->
+
+                        <div class="__txt">
+                            <span>Vinícius Fernandes</span>
+                            <span class="small">Front End</span>
+                        </div><!--txt-->
+                    </a>
+
+                    <a href="">
+                        <div class="__img">
+                            <img src="img/person_ico.png" alt="">
+                        </div><!--__img-->
+
+                        <div class="__txt">
+                            <span>Vinícius Fernandes</span>
+                            <span class="small">Front End</span>
+                        </div><!--txt-->
+                    </a>
+
+                    <a href="">
+                        <div class="__img">
+                            <img src="img/person_ico.png" alt="">
+                        </div><!--__img-->
+
+                        <div class="__txt">
+                            <span>Vinícius Fernandes</span>
+                            <span class="small">Front End</span>
+                        </div><!--txt-->
+                    </a>
+                </div><!--__tooltip-->
+            </div><!--collaborators-->
+          </div><!--__display-->
+
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+     </div><!--container-->
+</section><!--portfolioDetails-->
+
+<?php include('footer.php'); ?>
